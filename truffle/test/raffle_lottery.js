510d3840af078892cfa23d922efc8fc08ff3a2aa
Thomas Butterfield <tom1@me.com>
2021-12-16 16:57:29 +0800

    test script

---
+++ truffle/test/raffle_lottery.js
@@ -0,0 +1,72 @@
+const raffleLottery = artifacts.require("raffleLottery");
+const { expectRevert } = require('../node_modules/@openzeppelin/test-helpers');
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("raffleLottery", function (accounts) {
+
+  const [contractOwner, participant1, participant2, participant3] = accounts;
+  let instance;
+
+  beforeEach(async () => {
+    instance = await raffleLottery.new();
+  });
+
+  it("Raffle Lottery Contract Deployed", async function () {
+    await raffleLottery.deployed();
+    return assert.isTrue(true);
+  });
+
+  // it("!", async function () {
+  //   ticketPrice = Number(await instance.ticketPrice())
+
+  //   await expectRevert.assertion(
+      
+  //     instance.enterLottery(10, { from: participant3, value: 10}),
+  //     'participant ended lottery...'
+  //   );
+  // });
+
+  it("is the Lottery Live", async function () {
+    await instance.isLotteryLive();
+    return assert.isTrue(true);
+  });
+
+  it("Participants Can Enter", async function () {
+    ticketPrice = Number(await instance.ticketPrice())
+    await instance.enterLottery(5, { from: participant1, value: 5*ticketPrice });
+    return assert.isTrue(true);
+  });
+
+  it("Jackpot increases by 99% of transaction value", async function () {
+    ticketPrice = Number(await instance.ticketPrice())
+    jackpotTotal = Number(await instance.jackpotTotal())
+    await instance.enterLottery(10, { from: participant1, value: 10*ticketPrice });
+
+    assert.equal(
+      Number(await instance.jackpotTotal()),
+      jackpotTotal + 0.99*(10*ticketPrice),
+      'jackpot not doesnt get added to correctly'
+    );
+  });
+
+  it("Owner can end lottery and declare winner", async function () {
+    instance.declareWinner(10, { from: contractOwner })
+    await instance.hasLotteryEnded();
+
+    return assert.isTrue(true)
+  });
+
+  it("Participants cannot end lottery and declare winner", async function () {
+    instance.declareWinner(10, { from: participant3 })
+
+    assert.equal(
+      await instance.hasLotteryEnded(),
+      false,
+      'participant ended lottery...'
+    );
+  });
+});
